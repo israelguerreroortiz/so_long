86b292303635623192640ba013c2d4395e973ef6
IsraelGuerrero <israelguerreroortiz83@gmail.com>
2025-06-23 19:40:17 +0200

    formateo para norminette

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iisraa11 <iisraa11@student.42.fr>          +#+  +:+       +#+        */
+/*   By: isrguerr <isrguerr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 20:13:32 by isrguerr          #+#    #+#             */
-/*   Updated: 2025/04/25 17:08:42 by iisraa11         ###   ########.fr       */
+/*   Updated: 2025/06/23 16:25:23 by isrguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strdup(const char *s)
-{	
+{
 	char	*new;
 	size_t	buff_size;
 
