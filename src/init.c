31ad3896738fff45c7fde7dcc6b1cc79778ee6ee
IsraelGuerrero <israelguerreroortiz83@gmail.com>
2025-06-24 14:23:33 +0200

    bugfix: ruta equivocada para imagen coleccionable

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: isrguerr <isrguerr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 17:47:40 by isrguerr          #+#    #+#             */
-/*   Updated: 2025/06/24 14:07:13 by isrguerr         ###   ########.fr       */
+/*   Updated: 2025/06/24 14:22:25 by isrguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	init_textures(t_game *game)
 	game->img_exit = mlx_xpm_file_to_image(game->mlx, "sprites/exit.xpm", &w,
 			&h);
 	game->img_collect = mlx_xpm_file_to_image(game->mlx,
-			"sprites/collectt.xpm", &w, &h);
+			"sprites/collect.xpm", &w, &h);
 	if (!game->img_wall || !game->img_floor || !game->img_player
 		|| !game->img_exit || !game->img_collect)
 		return (1);
